5bae02050e6e5219093188704010daa86825b92e
Yevhen Khreptun <khreptun.e@gmail.com>
2023-08-25 12:56:49 -0400

    Form validation

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -13,11 +13,26 @@ const ContactForm = () => {
     subject: "",
     message: "",
   });
-
+  const [showSuccess, setShowSuccess] = useState(false);
   const [errors, setErrors] = useState({});
 
+  const minFieldLength = {
+    name: 2,
+    subject: 50,
+    message: 500,
+  };
+
+  const isValidEmail = /\S+@\S+\.\S+/.test(userInfo.email);
+
   const isAllowSend =
-    userInfo.name && userInfo.email && userInfo.subject && userInfo.message;
+    userInfo.name &&
+    userInfo.name.length >= minFieldLength.name &&
+    userInfo.email &&
+    isValidEmail &&
+    userInfo.subject &&
+    userInfo.subject.trim().length < minFieldLength.subject &&
+    userInfo.message &&
+    userInfo.message.trim().length < minFieldLength.message;
 
   const handleUserInfo = (e) => {
     setUserInfo((curr) => ({
@@ -31,21 +46,30 @@ const ContactForm = () => {
 
     const errorMessages = {};
 
-    if (!userInfo.name.trim()) errorMessages.nameError = "Required";
-
-    if (!userInfo.email.trim()) {
-      errorMessages.emailError = "Required";
+    if (!userInfo.name.trim()) {
+      errorMessages.nameError = "Name is required";
+    } else if (userInfo.name.length < minFieldLength.name) {
+      errorMessages.nameError = "Name has to be more then then 1 char";
     }
 
     if (!userInfo.email.trim()) {
-      errorMessages.subjectError = "Required";
+      errorMessages.emailError = "Email is required";
+    } else if (!isValidEmail) {
+      errorMessages.emailError = "Not valid email";
     }
 
-    if (!userInfo.email.trim()) {
-      errorMessages.messageError = "Required";
+    if (!userInfo.subject.trim()) {
+      errorMessages.subjectError = "Subject is required";
+    } else if (userInfo.subject.trim().length > minFieldLength.subject) {
+      errorMessages.subjectError = `Subject has to be less then ${minFieldLength.subject} char`;
     }
 
-    console.log(errorMessages);
+    if (!userInfo.message.trim()) {
+      errorMessages.messageError = "Message is required";
+    } else if (userInfo.message.trim().length > minFieldLength.message) {
+      errorMessages.messageError = `Message has to be less then ${minFieldLength.message} char`;
+    }
+
     setErrors(errorMessages);
 
     if (isAllowSend) {
@@ -74,55 +98,83 @@ const ContactForm = () => {
         message: "",
       });
     }
+
+    if (isAllowSend) {
+      setShowSuccess(true);
+    } else {
+      setShowSuccess(false);
+    }
   };
 
   return (
     <form className="contacts__form form" ref={form} onSubmit={sendEmail}>
       <div className="form__top">
-        <input
-          onChange={handleUserInfo}
-          name="name"
-          style={{
-            borderBottomColor: errors.nameError ? "red" : "white",
-          }}
-          type="text"
-          placeholder="Name"
-        />
-        {errors.nameError && <span>{errors.nameError}</span>}
-        <input
-          onChange={handleUserInfo}
-          name="email"
-          type="email"
-          placeholder="Email"
-        />
+        <div className="input__field">
+          <input
+            className={errors.nameError ? "field-error" : ""}
+            onChange={handleUserInfo}
+            name="name"
+            value={userInfo.name}
+            type="text"
+            placeholder="Name"
+          />
+          {errors.nameError && (
+            <span className="error-message">{errors.nameError}</span>
+          )}
+        </div>
+        <div className="input__field">
+          <input
+            className={errors.emailError ? "field-error" : ""}
+            onChange={handleUserInfo}
+            name="email"
+            value={userInfo.email}
+            type="text"
+            placeholder="Email"
+          />
+          {errors.emailError && (
+            <span className="error-message">{errors.emailError}</span>
+          )}
+        </div>
       </div>
       <div className="form__middle">
-        <input
-          onChange={handleUserInfo}
-          name="subject"
-          type="text"
-          placeholder="Subject"
-        />
+        <div className="input__field">
+          <input
+            className={errors.subjectError ? "field-error" : ""}
+            onChange={handleUserInfo}
+            name="subject"
+            value={userInfo.subject}
+            type="text"
+            placeholder="Subject"
+          />
+          {errors.subjectError && (
+            <span className="error-message">{errors.subjectError}</span>
+          )}
+        </div>
       </div>
       <div className="form__bottom">
         <textarea
+          className={errors.messageError ? "field-error" : ""}
           onChange={handleUserInfo}
           name="message"
+          value={userInfo.message}
           cols="30"
           rows="10"
           placeholder="Message"
         ></textarea>
+        {errors.messageError && (
+          <span className="error-message">{errors.messageError}</span>
+        )}
       </div>
-      <button
+      <Button
         style={{
           cursor: isAllowSend ? "pointer" : "not-allowed",
         }}
-        className="button"
-        type="submit"
-      >
-        Send
-      </button>
-      {/* <Button dataText={`Send message`} buttonStyles="button"></Button> */}
+        dataText={`Send message`}
+        buttonStyles="button"
+      ></Button>
+      {showSuccess && (
+        <p className="success-message">The message had been send!</p>
+      )}
     </form>
   );
 };
