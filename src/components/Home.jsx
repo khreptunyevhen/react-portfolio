859469ef151361ff330f670b2218ecd491a074b3
Yevhen Khreptun <khreptun.e@gmail.com>
2023-09-04 21:02:10 -0400

    Added random advices

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,29 @@ import { Link } from "react-router-dom";
 // TODO: add picture to resume buttons
 
 import pdf from "../assets/Yevhen_Khreptun_CV.pdf";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [advice, setAdvice] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function getAdvice() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const data = await res.json();
+
+        setAdvice(data.slip.advice);
+        setIsLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getAdvice();
+  }, []);
+
   return (
     <section className="home">
       <div className="container home__inner">
@@ -24,6 +45,11 @@ const Home = () => {
             <Button dataText="Projects" buttonStyles="button"></Button>
           </Link>
         </div>
+        {isLoading ? (
+          <p>Loading advice...</p>
+        ) : (
+          <p className="home__advice">“{advice}”</p>
+        )}
       </div>
     </section>
   );
